f50069e52f1467e343abe2de781354845d22ce48
satish <satish@8cd9765f-63a3-c049-87a0-e2347ebfad47>
2020-06-09 07:44:44 +0000

    git-svn-id: https://localserver/svn/EPCModule@17 8cd9765f-63a3-c049-87a0-e2347ebfad47

--- BussinessLayer/EPCISBLL.cs
+++ BussinessLayer/EPCISBLL.cs
@@ -69,5 +69,28 @@ namespace BussinessLayer
 
             return EPCISDAL.InsertEPCISLog(ObjEPCIS, Objxml.ToString());
         }
+
+        public static List<EPCISDO> GetEPCIS_URN_RPO(string CustomerId, long RPO)
+        {
+            List<EPCISDO> ObjList = new List<EPCISDO>();
+            var list = EPCISDAL.GetEPCIS_URN_RPO(CustomerId, RPO);
+
+            ObjList = (from c in list
+                       select new EPCISDO
+                       {
+                           CustomerId = c.CustomerId,
+                           RPO = (long)c.RPO,
+                           EPC_urn = c.EPCIS,
+                           rpcDetailNumber = (long)c.DetailNumber,
+                           SerailStart = (long)c.SerailStart,
+                           SerailEnd = (long)c.SerailEnd,
+                           Qty = c.Qty,
+                           EPC = c.EPC
+
+                       }).ToList();
+
+
+            return ObjList;
+        }
     }
 }
